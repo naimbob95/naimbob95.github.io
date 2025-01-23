fabf37ac1dcc4dc44958f47873301f62d1c1fede
Amirul Naim <naimbob95@gmail.com>
2025-01-23 23:26:55 +0800

    Add GitHub Actions workflow for deployment to GitHub Pages and set site URL in configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://naimbob95.github.io'
   integrations: [react(),
     tailwind({
       applyBaseStyles: false,
